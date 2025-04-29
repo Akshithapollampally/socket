a0b3db949803844e13d9bcadab7cfac73a3d9ba2
Akshitha Pollampally <161572312+Akshithapollampally@users.noreply.github.com>
2025-04-29 15:35:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import express from "express";
 import http from "http";
 import { Server } from "socket.io";
 import cors from "cors";
-
+import { connectDB } from "./backend/config/db.js";
 import ticketRouter from "./backend/routes/ticketRoute.js"
 
 const app = express();
